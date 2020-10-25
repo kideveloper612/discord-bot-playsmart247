8a9a42ec70c7920e452a897fc7f041f9675edfef
kideveloper612 <51858897+kideveloper612@users.noreply.github.com>
2020-10-26 05:02:38 +0800

    complete

--- playsmart247.py
+++ playsmart247.py
@@ -1,16 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 import os
-import csv
+import discord
+import dotenv
 
 
-def get_request():
+dotenv.load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+
+def get_request(param):
     headers = {
         'User-Agent': 'Mozila/5.0',
     }
 
-    res = requests.request("GET", base_url, headers=headers)
+    res = requests.request("GET", base_url, headers=headers, params=param)
     return res
 
 
@@ -19,14 +24,18 @@ def parse(content):
 
 
 def write(lines):
+    lines.reverse()
     with open(file='result.txt', encoding='utf-8', mode='w') as file:
         for item in lines:
             file.write("%s\n" % item)
 
 
-def main():
+def main(pins):
+    param = {
+        'spins': pins
+    }
     numbers = []
-    response = get_request()
+    response = get_request(param)
     soup = parse(response.text)
     options = soup.find_all('script')
     for option in options:
@@ -40,11 +49,50 @@ def main():
                 if number_soup is not None:
                     number = number_soup.text.strip()
                     numbers.append(number)
-                    print(len(numbers))
     write(lines=numbers)
 
 
-
 if __name__ == '__main__':
-    base_url = 'https://playsmart247.com/auto-roulette/?spins=250'
-    main()
+    base_url = 'https://playsmart247.com/auto-roulette'
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user.name} has connected to Discord!')
+
+    @client.event
+    async def on_member_join(member):
+        await member.create_dm()
+        await member.dm_channel.send(
+            f'Hi {member.name}, welcome to my Discord server!'
+        )
+
+    @client.event
+    async def on_message(message):
+
+        print('{} requested to scrape'.format(message.author))
+        if message.author == client.user:
+            return
+
+        if '.help' in message.content:
+             await message.channel.send("""
+                 Pour m'utiliser, c'est tres simple : 
+                - Si vous rechercher 250 entrer:
+                .send 250 spin
+                
+                - Si vous rechercher 25000 spin enter: 
+                .send 2500 spin""")
+
+        if '.send 250 spin' in message.content:
+            main(pins=250)
+            await message.channel.send(" Un moment je scan pour vous la table bleu ")
+            await message.channel.send(file=discord.File('result.txt'))
+        elif '.send 2500 spin' in message.content:
+            main(pins=2500)
+            await message.channel.send(" Un moment je scan pour vous la table bleu ")
+            await message.channel.send(file=discord.File('result.txt'))
+
+        elif message.content == 'raise-exception':
+            raise discord.DiscordException
+
+    client.run(TOKEN)
